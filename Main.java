e0d83816f283fcaa5b1d106553117d5939a70b15
Vladimir Zubko <zburguy@gmail.com>
2016-09-13 18:33:26 +0300

    Я добавил новый метод

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
 	public static void main(String[] args) {
 		String str = "My";
 		System.out.println("Hello World!!!" + str);
+		print();
+	}
+	
+	static void print() {
+		System.out.println("New String");
 	}
 }
